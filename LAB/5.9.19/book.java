a9cda4b55832f9387ea3280d78ded3d6de4949df
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-09-12 19:59:33 +0530

    Update book.java

--- LAB/5.9.19/book.java
+++ LAB/5.9.19/book.java
@@ -1,97 +1,53 @@
+//Class book - 4 members: name, author , price, no. of pages. Include a constructor
+// to set the values for the members include methods to set and get details 
+// of the object. Include two string methods that display complete details of 
+//the book. WAP to create and create a book objects
+
 import java.util.*;
-
-class person
+class book
 {
-    Scanner s= new Scanner(System.in);
-    int age;
-	String name,address;
-	person(){}
-    void input()
-    {
-        System.out.println("\nEnter the Name:");
-        name=s.next();
-        System.out.println("\nEnter the Address:");
-        address=s.next();
-        //this.name=name;
-        // this.address=address;
+	Scanner s = new Scanner(System.in);
+	String name,author;
+	int price,pages;
+	book()
+	{
+		price = 100;
+		pages = 500;
+		name = "The Power of Six";
+		author = "Pittacus Lore";
+	}
+	void setdata()
+	{
+		System.out.println("\n ---Enter the Book data---");
+		System.out.println("\n Enter the Name: ");
+		name = s.next();
+		System.out.println("\n Enter the Author's Name: ");
+		author = s.next();
+		System.out.println("\n Enter the Price:");
+		price = s.nextInt();
+		System.out.println("\n Enter the Pages:");
+		pages = s.nextInt();
+	}
+	public String toString()
+	{
+		return("\n NAME:"+name+"\nAUTHOR:"+author+"\nPAGES:"+pages+"\nPRICE:"+price); 
     }
 }
 
-class student extends person
+class bookmain
 {
-	int rollno,sem;
-	Scanner s= new Scanner(System.in);
-	student(){}
-    void input()
-    {
-        System.out.println("\nEnter the RollNo:");
-        rollno=s.nextInt();
-        System.out.println("\nEnter the Sem:");
-        sem=s.nextInt();
-        super.input();
-        //this.rollno=rollno;
-        //this.sem=sem;
-        //super(name,address);
-    }
-}
-
-class exam extends student
-{
-    int n,marks1,marks2;
-	float avg;
-	exam(){}
-    void input()
-    {
+	public static void main(String xx[])
+	{
 		Scanner s= new Scanner(System.in);
-        System.out.println("\nEnter Marks 1:");
-        marks1=s.nextInt();
-        System.out.println("\nEnter Marks 2:");
-        marks2=s.nextInt();
-        super.input();
-        //this.marks1=marks1;
-        //this.marks2=marks2;
-        //super(rollno,sem,name,address);
-    }
-    float calc()
-    {
-        avg=(marks1+marks2)/2;
-        return avg;
-    }
-    void display()
-    {
-        System.out.println("\n The topper is:"+name+"\n roll no:"+rollno+"\nsem:"+sem+"\nAddress:"+address);
-    }
-}
-
-class classmain
-{
-    public static void main(String xx[])
-    {
-        Scanner s= new Scanner(System.in);
-        int n,k=0;
-        System.out.println("\nEnter the no. of students:");
-        n=s.nextInt();
-        exam s1[]= new exam[n];
-        for(int i=0; i<n; i++)
+		System.out.println("\nEnter the number of books:");
+		int n = s.nextInt();
+		book b1[] = new book[n];
+		for(int i=0; i<n; i++)
 		{
-			s1[i]=new exam();
-            s1[i].input();
-        }
-        for(int i=0; i<n; i++)
-        {
-            if(i==n-1)
-            {
-                break;
-            }
-            else if(s1[i].calc() > s1[i+1].calc())
-            {
-                k=i;
-            }
-            else
-            {
-                k=i+1;
-            }
-        }
-        s1[k].display();
-    }
-}
+			b1[i] = new book();
+			b1[i].setdata();
+			System.out.println(b1[i]);
+
+		}
+	}
+}	 
