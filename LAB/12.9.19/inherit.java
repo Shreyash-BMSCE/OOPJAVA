e55a4b77961048d75ed9cc13c5611f9ec09a7a83
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-09-12 19:55:10 +0530

    Update inherit.java

--- LAB/12.9.19/inherit.java
+++ LAB/12.9.19/inherit.java
@@ -2,75 +2,96 @@ import java.util.*;
 
 class person
 {
-    Scanner s= new Scanner;
+    Scanner s= new Scanner(System.in);
     int age;
-    string name,address;
+	String name,address;
+	person(){}
     void input()
     {
-        system.out.println("\nEnter the Name:");
+        System.out.println("\nEnter the Name:");
         name=s.next();
-        system.out.println("\nEnter the Address:");
+        System.out.println("\nEnter the Address:");
         address=s.next();
         //this.name=name;
         // this.address=address;
     }
 }
 
-class student extend person
+class student extends person
 {
-    int rollno,sem;
+	int rollno,sem;
+	Scanner s= new Scanner(System.in);
+	student(){}
     void input()
     {
-        system.out.println("\nEnter the RollNo:");
+        System.out.println("\nEnter the RollNo:");
         rollno=s.nextInt();
-        system.out.println("\nEnter the Sem:");
+        System.out.println("\nEnter the Sem:");
         sem=s.nextInt();
-        super input();
+        super.input();
         //this.rollno=rollno;
         //this.sem=sem;
         //super(name,address);
     }
 }
 
-class exam extend student
+class exam extends student
 {
     int n,marks1,marks2;
-    float avg;
+	float avg;
+	exam(){}
     void input()
     {
-        system.out.println("\nEnter Marks 1:");
+		Scanner s= new Scanner(System.in);
+        System.out.println("\nEnter Marks 1:");
         marks1=s.nextInt();
-        system.out.println("\nEnter Marks 2:");
+        System.out.println("\nEnter Marks 2:");
         marks2=s.nextInt();
-        super input();
+        super.input();
         //this.marks1=marks1;
         //this.marks2=marks2;
         //super(rollno,sem,name,address);
     }
-    void calc()
+    float calc()
     {
         avg=(marks1+marks2)/2;
+        return avg;
     }
     void display()
     {
-        system.out.println("\n The topper is:"+name+"\n roll no:"+rollno+"\nsem:"+sem+"\nAddress:"+address);
+        System.out.println("\n The topper is:"+name+"\n roll no:"+rollno+"\nsem:"+sem+"\nAddress:"+address);
     }
 }
 
 class classmain
 {
-    public static void main(string xx[])
+    public static void main(String xx[])
     {
-        Scanner s=new Scanner;
-        int n;
-        system.out.println("\nEnter the no. of students:");
+        Scanner s= new Scanner(System.in);
+        int n,k=0;
+        System.out.println("\nEnter the no. of students:");
         n=s.nextInt();
         exam s1[]= new exam[n];
         for(int i=0; i<n; i++)
 		{
-			
-			
-		}
-
+			s1[i]=new exam();
+            s1[i].input();
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(i==n-1)
+            {
+                break;
+            }
+            else if(s1[i].calc() > s1[i+1].calc())
+            {
+                k=i;
+            }
+            else
+            {
+                k=i+1;
+            }
+        }
+        s1[k].display();
     }
 }
